cacd4412ab4073dffb826d3e3a79fa6d75e7c8b1
Sev6n <94166680+Sev6nOfficial@users.noreply.github.com>
2022-09-14 21:54:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandType, ButtonStyle, SelectMenuBuilder } = require('discord.js');
+const translate = require("@iamtraction/google-translate")///npm i @iamtraction/google-translate
+//translate command in trenny
+//just give some credit or copyright
+//https://trenny.tk/support.html
+module.exports = {
+	name: 'translate',
+  description: "Translates any text into English",
+  usage: "translate",
+  category: "freemium",
+	userPerms: ['VIEW_CHANNELS', 'SEND_MESSAGES],
+	botPerms: ['ADMINISTRATOR'],
+	cooldown: 3,
+  guildOnly: false,
+  ownerOnly: false,
+  toggleOff: false,
+  nsfwOnly: false,
+  maintenance: false,
+	type: ApplicationCommandType.ChatInput,
+   options: [
+		{
+			name: "text",
+			description: "The Sentence To Translate",
+			type: 3,
+			required: true,
+		}, 
+   ],
+  
+	run: async (client, interaction) => {
+ 
+    try{ 
+       const query = interaction.options.getString("text");
+      const translated = await translate(query, { to: 'en' })
+
+        const transEmbed = new EmbedBuilder()
+            
+           .setColor(0x0099FF)
+          .addFields(
+		{ name: 'Text To Translate', value: "```" + query + "```" },
+		{ name: 'Translated Text', value: "```" + translated.text + "```" },
+		
+	)
+            .setTimestamp()
+      	.setFooter({ text: 'Milenium - The Best Discord Bot Premium alternative since december 2021!', iconURL: 'iconurl' });
+
+        interaction.reply({ embeds: [transEmbed] })
+    } catch (error){
+      client.slash_err(client, interaction, error);
+    }
+	}
+};
